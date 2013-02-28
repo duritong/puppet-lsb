241bd4dee102df3db84f84ab2b47dabd775ec161
mh <mh@immerda.ch>
2013-02-28 22:16:39 +0100

    on el6 -core is sufficient

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,5 +1,10 @@
+# manage EL based distros
 class lsb::centos inherits lsb::base {
+  $package_name = $::lsbmajdistrelease ? {
+    5       => 'redhat-lsb',
+    default => 'redhat-lsb-core'
+  }
   Package['lsb']{
-    name => 'redhat-lsb',
+    name => $package_name,
   }
 }
